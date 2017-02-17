55495c0a9916ddef1f5b616d1f85e8d9d6b6cac8
Bruno Ribeiro <bruno.rs.ribeiro@ua.pt>
2017-02-17 20:11:12 +0000

    Aula02 Unknow syscall on READ_CORE_TIMER

---
+++ Aula02/prog1.s
@@ -0,0 +1,23 @@
+        .equ READ_CORE_TIMER,11
+        .equ RESET_CORE_TIMER,12
+        .equ PUT_CHAR,3
+        .equ PRINT_INT,6
+        .data
+        .text
+        .globl main
+main:   ori $t0,$0,0
+while:  ori $v0,READ_CORE_TIMER
+#        syscall
+        bge $v0,200000,endw
+        ori $v0,$0,RESET_CORE_TIMER
+        syscall
+        li $a0,32
+        ori $v0,$0,PUT_CHAR
+        syscall
+        addi $t0,$t0,1
+        or $a0,$t0,$0
+        ori $a1,$0,10
+        ori $v0,$0,PRINT_INT
+        syscall
+        j while
+endw:   jr $ra
